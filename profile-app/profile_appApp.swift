220fbda4f5b58c2b39281364b1398d6a1e384f52
Jay-Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2024-10-02 11:55:59 +0530

    Initial Commit

---
+++ profile-app/profile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_appApp.swift
+//  profile-app
+//
+//  Created by Jay Tillu on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct profile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
